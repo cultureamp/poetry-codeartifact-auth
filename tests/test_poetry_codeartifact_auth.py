6651bf458dc2bf0a3b15242b9b1f499a18062dbd
andy.mackinlay <andy.mackinlay@cultureamp.com>
2022-10-03 09:21:35 +1100

    fixed tests

--- tests/test_poetry_codeartifact_auth.py
+++ tests/test_poetry_codeartifact_auth.py
@@ -1,10 +1,14 @@
+# pylint:disable=missing-docstring
 import pytest
 
-from poetry_codeartifact_auth import CodeArtifactRepoConfig, parse_poetry_repo_config
+from poetry_codeartifact_auth import (
+    CodeArtifactRepoConfig,
+    parse_poetry_repo_config,
+    CodeArtifactUrlParseException,
+)
 
 
 class TestCodeArtifactRepoConfig:
-
     @staticmethod
     def test_from_url_works_with_good_value():
         result = CodeArtifactRepoConfig.from_url(
@@ -21,7 +25,7 @@ class TestCodeArtifactRepoConfig:
     def test_from_url_throws_valueerror_with_bad_value():
         with pytest.raises(ValueError):
             CodeArtifactRepoConfig.from_url("garbage")
-        with pytest.raises(ValueError):
+        with pytest.raises(CodeArtifactUrlParseException):
             CodeArtifactRepoConfig.from_url(
                 "https://example-domain-1234567.d.INVALID.us-west-2.amazonaws.com/some-suffix"
             )
@@ -29,4 +33,4 @@ class TestCodeArtifactRepoConfig:
 
 def test_parse_poetry_repo_config():
     config_output = "{'example': {'url': 'https://repo.example.com'}}"
-    assert parse_poetry_repo_config(config_output)['example']['url'] == 'https://repo.example.com'
+    assert parse_poetry_repo_config(config_output)["example"]["url"] == "https://repo.example.com"
